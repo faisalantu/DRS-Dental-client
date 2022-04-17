e555b4078a69765e15e8c3d8abb5933e489dcc3e
faisalantu <faisalantu@gmail.com>
2022-04-18 03:06:52 +0600

    📱 resoonsive navbar done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,48 @@
-import React from "react";
+import React, { useState } from 'react';
 import CustomLink from "../CustomLink/CustomLink";
 import { FaTooth } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return ( 
-    <nav className="flex bg-blue-50 text-blue-900 justify-between p-4">
+    <nav className="bg-blue-50 text-blue-900 ">
+      {/*💻 for pc device  */}
+      <div className='justify-between p-4 hidden md:flex'>
         <div className="font-semibold text-xl flex items-center">
-           <span className="mx-2"><FaTooth/></span> <span>DRS Dental</span>
+          <span className="mx-2"><FaTooth/></span> <span>DRS Dental</span>
         </div>
-      <div className="flex justify-center items-center">
-        <CustomLink className="mx-1 px-1" to='/'>Home</CustomLink>
-        <CustomLink className="mx-1 px-1" to='/checkout'>Checkout</CustomLink>
-        <CustomLink className="mx-1 px-1" to='/blogs'>Blogs</CustomLink>
-        <CustomLink className="mx-1 px-1" to='/about'>About</CustomLink>
+        <div className="flex justify-center items-center">
+          <CustomLink className="mx-1 px-1" to='/'>Home</CustomLink>
+          <CustomLink className="mx-1 px-1" to='/checkout'>Checkout</CustomLink>
+          <CustomLink className="mx-1 px-1" to='/blogs'>Blogs</CustomLink>
+          <CustomLink className="mx-1 px-1" to='/about'>About</CustomLink>
+        </div>
+        <div className="flex justify-center items-center">
+          <CustomLink className="mx-1 px-1" to='/signin'>Sign in</CustomLink>
+        </div>
       </div>
-      <div className="flex justify-center items-center">
-        <CustomLink className="mx-1 px-1" to='/signin'>Sign in</CustomLink>
+
+      {/* 📱 for mobile device */}
+      <div className='p-4 md:hidden'>
+        <div className='flex justify-between'>
+          <div className="font-semibold text-xl flex items-center">
+            <span className="mx-2"><FaTooth/></span> <span>DRS Dental</span>
+          </div>
+            <button onClick={()=>{
+              setMenuOpen(!menuOpen)
+            }} className='text-xl'>
+             <GiHamburgerMenu/>  
+            </button>
+        </div>
+          {menuOpen?
+          <div className="flex justify-center flex-col items-center mt-4">
+            <CustomLink className="mx-1 px-1" to='/'>Home</CustomLink>
+            <CustomLink className="mx-1 px-1" to='/checkout'>Checkout</CustomLink>
+            <CustomLink className="mx-1 px-1" to='/blogs'>Blogs</CustomLink>
+            <CustomLink className="mx-1 px-1" to='/about'>About</CustomLink>
+            <CustomLink className="mx-1 px-1" to='/signin'>Sign in</CustomLink>
+          </div>
+          : ""}
       </div>
     </nav>
   );
