374a58a3953eecce666e7e84a776ce8a64a9412e
faisalantu <faisalantu@gmail.com>
2022-04-18 21:49:51 +0600

    🪧 wrote more about myself

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,9 +4,14 @@ const About = () => {
   return (
     <div className='container mx-auto p-7'>
       <h1 className='text-center font-semibold text-2xl mb-5'>Faisal Ahmed</h1>
-      <div className="w-7/12 mx-auto text-center">
+      <div className='w-7/12 mx-auto text-center'>
         <p>
-          I am Faisal Ahmed, still student of Green University of Bangladesh. I used to do competitive programming, now I want to learn web technologies. my future goal is to master Full-Stack web development and find remote job or freelance so that i can work fom home and give time to my parents.
+          I am Faisal Ahmed, still student of Green University of Bangladesh. I
+          used to do competitive programming, now I want to learn web
+          technologies. my future goal is to master Full-Stack web development
+          and find remote job or freelance so that I can work fom home and give
+          time to my parents. To achive my goal I will solve problems and learn
+          more about all the web technologies.
         </p>
       </div>
     </div>
